3b4880007e18a027220c778d149e6c69cea0ce26
Cronus-waters <cronuswaters@gmail.com>
2025-07-13 13:11:39 -0300

    Added Minesweeper Trophy

---
+++ Content/Items/MinesweeperTrophy.cs
@@ -0,0 +1,26 @@
+﻿using JulyJam.Content.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JulyJam.Content.Items
+{
+    public class MinesweeperTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<MinesweeperTrophyTile>());
+            Item.value = Item.sellPrice(gold: 1);
+            Item.rare = ItemRarityID.Blue;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient<MinedMine>(99);
+            recipe.AddTile(TileID.HeavyWorkBench);
+            recipe.Register();
+
+        }
+    }
+}
