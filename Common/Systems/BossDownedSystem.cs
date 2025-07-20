d9e841602b1d49e04182e8f985dd0ff06fd791d9
emyhrberg <erik.myhrberg@hotmail.com>
2025-07-20 06:26:02 +0200

    Started nine boss (wip) - Most things arent working at all. Only follow player is working. Started with sprites. Todo: - fix attacks (mine spawner/shooter, mine rain, teleporter, maybe more) - fix AI - fix sprites - add boss bag and summon scenario or item

---
+++ Common/Systems/BossDownedSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace TutorialMod.Common.Systems
+{
+    public class BossDownedSystem : ModSystem
+    {
+        public static bool downedNineBoss = false;
+
+        public override void ClearWorld()
+        {
+            downedNineBoss = false;
+        }
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            tag["downedNineBoss"] = downedNineBoss;
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            downedNineBoss = tag.GetBool("downedNineBoss");
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            var flags = new BitsByte();
+            flags[0] = downedNineBoss;
+            writer.Write(flags);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            BitsByte flags = reader.ReadByte();
+            downedNineBoss = flags[0];
+        }
+    }
+}
